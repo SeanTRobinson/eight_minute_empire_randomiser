415a3ef66077e9ef1a7f71ab85ac4c03456ed3af
Sean Robinson <Sean.Robinson@LexAble.com>
2015-07-30 21:12:13 +0100

    Working now

--- EightMinuteEmpireLegends-Randomiser/app/src/main/java/com/seanrobinson/eightminuteempirelegends_randomiser/MainActivity.java
+++ EightMinuteEmpireLegends-Randomiser/app/src/main/java/com/seanrobinson/eightminuteempirelegends_randomiser/MainActivity.java
@@ -1,10 +1,10 @@
 package com.seanrobinson.eightminuteempirelegends_randomiser;
 
 import android.content.res.AssetManager;
+import android.graphics.Bitmap;
+import android.graphics.drawable.Drawable;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
@@ -12,6 +12,7 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import java.io.IOException;
+import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
@@ -41,7 +42,7 @@ public class MainActivity extends ActionBarActivity {
         skipButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                setNewImage(false);
+                setNewImage(true);
             }
         });
     }
@@ -59,8 +60,11 @@ public class MainActivity extends ActionBarActivity {
         AssetManager assetManager = getAssets();
         String[] possibleLocations;
         try {
-            possibleLocations = assetManager.list("locations");
+            possibleLocations = assetManager.list("");
             remainingLocations = new ArrayList<String>(Arrays.asList(possibleLocations));
+            remainingLocations.remove("images");
+            remainingLocations.remove("sounds");
+            remainingLocations.remove("webkit");
             long seed = System.nanoTime();
             Collections.shuffle(remainingLocations, new Random(seed));
         } catch (IOException e) {
@@ -117,11 +121,23 @@ public class MainActivity extends ActionBarActivity {
     private void setNewImage(boolean removeImage) {
         closeIfNoMoreImages();
 
-        int resID = getResources().getIdentifier(remainingLocations.get(0), "drawable", getPackageName());
-        locationImageView.setImageResource(resID);
+        setNewDrawable(remainingLocations.get(0));
 
         if (removeImage) {
             remainingLocations.remove(0);
+        }
+    }
+
+    private void setNewDrawable(String filename) {
+        try {
+            InputStream ims = getAssets().open(filename);
+            Drawable d = Drawable.createFromStream(ims, null);
+            locationImageView.setImageDrawable(d);
+        }
+        catch(IOException ex)
+        {
+            Toast.makeText(this, "Could not set new drawable.", Toast.LENGTH_SHORT);
+            finish();
         }
     }
 
